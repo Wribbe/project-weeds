8e12940c1f27745082e36491d8056cdb2566f7b0
Stefan Eng <eng_steff@outlook.com>
2022-01-05 16:14:00 +0100

    feat: Use ModelPerspectiveView matrix defaulted to identity matrix

--- src/main.c
+++ src/main.c
@@ -136,8 +136,9 @@ main(int argc, char * argv[])
   const char * source_shader_vertex = \
     "#version 330 core\n"
     "layout (location=0) in vec3 aPos;\n"
+    "uniform mat4 MPV = mat4(1.0f);\n"
     "void main() {\n"
-    "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "  gl_Position = MPV * vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "};";
 
   GLuint shader_vertex = glCreateShader(GL_VERTEX_SHADER);
